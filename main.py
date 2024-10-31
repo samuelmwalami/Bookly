b04e7576d32f7cc337f9ed5c8c1aa6ebe80ae0d0
Samuel Mwalami <samuelmwalami@gmail.com>
2024-10-31 18:49:18 +0300

    Added delete implementation

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import json
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+"""Book data models """
+#Get book response model
+class Book(BaseModel):
+    id : int
+    title : str
+    author : str
+    publisher : str
+    published_date : str
+    page_count : int
+    language : str
+    
+#Update book model
+class UpdateBook(BaseModel):
+    title : str
+    author : str
+    publisher : str
+    page_count : str
+    language : str
+    
+
+
+def load_books() -> dict:
+    if os.path.exists("books.json"):
+        with open("books.json","r") as f:
+           books = json.loads(f.read())
+    return books
+books = load_books()
+
+@app.get("/books", response_model=List[Book] ,status_code=status.HTTP_200_OK)
+async def get_books():
+    return books
+
+@app.get("/book/{book_id}",response_model=Book,status_code=status.HTTP_200_OK)
+async def get_book(book_id:int) -> dict: 
+    for book in books:
+        if book["id"] == book_id:
+            my_book = book
+    return my_book
+
+@app.post("/book",response_model=Book,status_code=status.HTTP_201_CREATED)
+async def add_book(book:Book):
+    new_book = book.model_dump()
+    return new_book
+
+@app.patch("/book/{book_id}")
+async def update_book(book:UpdateBook,book_id:int) -> dict:
+    book_update = book.model_dump()
+    books = load_books()
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = book_update["title"]            
+            book["author"] = book_update["author"]
+            book["publisher"] = book_update["publisher"]
+            book["page_count"] = book_update["page_count"]
+            book["language"] = book_update["language"]
+            updated_book = book
+    return book
+
+@app.delete("/book/{book_id}")
+async def delete_book() -> dict:
+    pass
